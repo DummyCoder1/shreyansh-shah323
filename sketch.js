47dc523a689b4a40adda95f65efba32dd12a23fb
DummyCoder1 <100186733+DummyCoder1@users.noreply.github.com>
2022-07-20 10:04:30 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,21 @@
+var friends = ["tarun","harshit","vedanshu","abhinav","sourabh"]
+
+function setup() {
+  createCanvas(innerWidth, innerHeight);
+  background("white");
+  console.log("My best friends are :");
+for(i=0; i <friends.length; i++){
+  console.log("tarun","harshit","vedanshu","abhinav","sourabh"[i]);
+}
+  //2. print the names in friends array in the console:
+
+
+
+}
+
+function draw() {
+    
+}
+
+    
+
